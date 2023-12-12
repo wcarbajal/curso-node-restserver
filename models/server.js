141f89b080b26fae6b01e0154a054797812461cf
Wuilmer Carbajal <38920693+wcarbajal@users.noreply.github.com>
2023-12-12 12:12:43 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 3000;
+    this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
       
     //Middlewares
